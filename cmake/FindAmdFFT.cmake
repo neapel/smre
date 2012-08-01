7673e0dee68eb14baf736710a87e61685149fb63
Pascal Germroth <pascal@ensieve.org>
2012-08-01 19:37:40 +0200

    find amd fft

--- cmake/FindAmdFFT.cmake
+++ cmake/FindAmdFFT.cmake
@@ -7,36 +7,36 @@
 
 include(FindPackageHandleStandardArgs)
 
-find_path(AmdFFT_ROOT_DIR
+find_path(AMDFFT_ROOT_DIR
 	NAMES include/clAmdFft.h
-	HINTS ${AmdFFT_ROOT_DIR}
+	HINTS ${AMDFFT_ROOT_DIR}
 	ENV AMDFFTROOT
 )
 
 # Include dir
-find_path(AmdFFT_INCLUDE_DIR
+find_path(AMDFFT_INCLUDE_DIR
 	NAMES clAmdFft.h
-	HINTS ${AmdFFT_ROOT_DIR}/include
+	HINTS ${AMDFFT_ROOT_DIR}/include
 )
 
 # Library
-find_library(AmdFFT_LIBRARIES
+find_library(AMDFFT_LIBRARY
 	NAMES clAmdFft.Runtime
-	HINTS ${AmdFFT_ROOT_DIR}/lib64 ${AmdFFT_ROOT_DIR}/lib32 
+	HINTS ${AMDFFT_ROOT_DIR}/lib64 ${AMDFFT_ROOT_DIR}/lib32 
 )
 
 # Version
-set(AmdFFT_VERSION_FILE ${AmdFFT_INCLUDE_DIR}/clAmdFft.version.h)
-if(EXISTS ${AmdFFT_VERSION_FILE})
-	file(STRINGS ${AmdFFT_VERSION_FILE} AmdFFT_MAJOR REGEX "#define[ ]+clAmdFftVersionMajor[ ]+[0-9]+")
-	file(STRINGS ${AmdFFT_VERSION_FILE} AmdFFT_MINOR REGEX "#define[ ]+clAmdFftVersionMinor[ ]+[0-9]+")
-	file(STRINGS ${AmdFFT_VERSION_FILE} AmdFFT_PATCH REGEX "#define[ ]+clAmdFftVersionPatch[ ]+[0-9]+")
-	string(REGEX MATCH "[0-9]+" AmdFFT_MAJOR ${AmdFFT_MAJOR})
-	string(REGEX MATCH "[0-9]+" AmdFFT_MINOR ${AmdFFT_MINOR})
-	string(REGEX MATCH "[0-9]+" AmdFFT_PATCH ${AmdFFT_PATCH})
-	set(AmdFFT_VERSION "${AmdFFT_MAJOR}.${AmdFFT_MINOR}.${AmdFFT_PATCH}")
+set(AMDFFT_VERSION_FILE ${AMDFFT_INCLUDE_DIR}/clAmdFft.version.h)
+if(EXISTS ${AMDFFT_VERSION_FILE})
+	file(STRINGS ${AMDFFT_VERSION_FILE} AMDFFT_MAJOR REGEX "#define[ ]+clAmdFftVersionMajor[ ]+[0-9]+")
+	file(STRINGS ${AMDFFT_VERSION_FILE} AMDFFT_MINOR REGEX "#define[ ]+clAmdFftVersionMinor[ ]+[0-9]+")
+	file(STRINGS ${AMDFFT_VERSION_FILE} AMDFFT_PATCH REGEX "#define[ ]+clAmdFftVersionPatch[ ]+[0-9]+")
+	string(REGEX MATCH "[0-9]+" AMDFFT_MAJOR ${AMDFFT_MAJOR})
+	string(REGEX MATCH "[0-9]+" AMDFFT_MINOR ${AMDFFT_MINOR})
+	string(REGEX MATCH "[0-9]+" AMDFFT_PATCH ${AMDFFT_PATCH})
+	set(AMDFFT_VERSION "${AMDFFT_MAJOR}.${AMDFFT_MINOR}.${AMDFFT_PATCH}")
 endif()
 
-find_package_handle_standard_args(AmdFFT
-	REQUIRED_VARS AmdFFT_INCLUDE_DIR AmdFFT_LIBRARIES
-	VERSION_VAR AmdFFT_VERSION)
+find_package_handle_standard_args(AMDFFT
+	REQUIRED_VARS AMDFFT_INCLUDE_DIR AMDFFT_LIBRARY
+	VERSION_VAR AMDFFT_VERSION)
