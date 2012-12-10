23fdbd3068f41c6468f21912f7f7e4d8d5261f1c
Pascal Germroth <pascal@ensieve.org>
2012-12-10 17:19:38 +0100

    cmake options

--- src/gil_io.h
+++ src/gil_io.h
@@ -2,11 +2,19 @@
 #define GIL_IO_H_
 
 #include "smre.h"
-#ifdef HAVE_GIL
 
 boost::multi_array<float, 2> read_image(std::string);
 void write_image(std::string, const boost::multi_array<float, 2> &);
 
+#ifndef HAVE_GIL
+#include <stdexcept>
 
+boost::multi_array<float, 2> read_image(std::string) {
+	throw std::runtime_error("Compiled without GIL, can't read images.");
+}
+void write_image(std::string, const boost::multi_array<float, 2> &) {
+	throw std::runtime_error("Compiled without GIL, can't write images.");
+}
 #endif
+
 #endif
